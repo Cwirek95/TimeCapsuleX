65303c936bb9db5eab1955caacad61cf29fb3598
Andrzej Ćwiertniak <acwiertniak95@gmail.com>
2025-01-13 19:11:02 +0100

    feat: create home dashboard content

--- app/src/app/dashboard/home/dashboard-home.component.ts
+++ app/src/app/dashboard/home/dashboard-home.component.ts
@@ -3,7 +3,36 @@ import { Component } from "@angular/core";
 @Component({
   selector: "app-dashboard-home",
   imports: [],
-  template: ` <p>dashboard-home works!</p> `,
+  template: `
+    <section class="mt-6 mb-6 mx-8">
+      <div class="stats-cards flex flex-col gap-2 sm:flex-row sm:justify-between">
+        <div class="stats-card">
+          <h3>Time-Based Capsules</h3>
+          <p>23</p>
+        </div>
+        <div class="stats-card">
+          <h3>Conditional Capsules</h3>
+          <p>8</p>
+        </div>
+        <div class="stats-card">
+          <h3>Inheritable Capsules</h3>
+          <p>0</p>
+        </div>
+        <div class="stats-card">
+          <h3>Group-Based Capsules</h3>
+          <p>11</p>
+        </div>
+        <div class="stats-card">
+          <h3>Message In a Bottle Capsules</h3>
+          <p>9</p>
+        </div>
+      </div>
+
+      <div class="activities-card mt-6">
+        <h3 class="font-semibold text-center">No new activities</h3>
+      </div>
+    </section>
+  `,
   standalone: true,
   styleUrls: ["dashboard-home.component.scss"],
 })
