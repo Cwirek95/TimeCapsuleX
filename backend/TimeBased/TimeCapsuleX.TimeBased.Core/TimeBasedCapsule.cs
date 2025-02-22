c06aa8c98d2d32aabdb5c2a001d8bd57b7fe9050
Cwirek95 <acwiertniak95@gmail.com>
2025-02-22 17:14:57 +0100

    feat: init TimeBased capsule core

---
+++ backend/TimeBased/TimeCapsuleX.TimeBased.Core/TimeBasedCapsule.cs
@@ -0,0 +1,29 @@
+﻿namespace TimeCapsuleX.TimeBased.Core;
+
+public sealed class TimeBasedCapsule
+{
+    public Guid Id { get; init; }
+
+    public Guid UserId { get; init; }
+    
+    public DateTimeOffset OpeningDate { get; init; }
+    
+    public string Title { get; init; }
+    
+    public string Message { get; init; }
+    
+    public DateTimeOffset CreatedAt { get; init; }
+    public DateTimeOffset? OpenedAt { get; private set; }
+    
+    public bool IsOpened => OpenedAt.HasValue;
+    
+    
+    private TimeBasedCapsule(Guid id, Guid userId, DateTimeOffset openingDate, string title, string message)
+    {
+        Id = id;
+        UserId = userId;
+        OpeningDate = openingDate;
+        Title = title;
+        Message = message;
+    }
+}
