b75a7c1ad8e8a86943ca6a92835cfb42010f9350
Cwirek95 <acwiertniak95@gmail.com>
2025-03-07 20:48:47 +0100

    feat: init common infrastructure with basic module
    Add ClockModule and MediatorModule to Common Infrastructure

---
+++ backend/Common/TimeCapsuleX.Common.Infrastructure/Clock/ClockModule.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace TimeCapsuleX.Common.Infrastructure.Clock;
+
+internal static class ClockModule
+{
+    internal static IServiceCollection AddClock(this IServiceCollection services) =>
+        services.AddSingleton(TimeProvider.System);
+}
---
+++ backend/Common/TimeCapsuleX.Common.Infrastructure/Mediator/MediatorModule.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace TimeCapsuleX.Common.Infrastructure.Mediator;
+
+public static class MediatorModule
+{
+    public static IServiceCollection AddMediator(this IServiceCollection services, Assembly assembly)
+    {
+        services.AddMediatR(configuration => configuration.RegisterServicesFromAssembly(assembly));
+
+        return services;
+    }
+}
